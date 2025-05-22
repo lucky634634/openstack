831b411c90c3420d88b35d1d026c0ea219a9ce99
lucky634634 <khoa21102003@gmail.com>
2025-05-22 19:27:35 +0700

    Update

--- backend/main.py
+++ backend/main.py
@@ -3,5 +3,13 @@ import uvicorn
 
 app = FastAPI(debug=True)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8080) 
