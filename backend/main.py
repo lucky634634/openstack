59a7d4ebba67472546b9bae2f380d8efe807a0fd
lucky634634 <khoa21102003@gmail.com>
2025-05-26 20:30:48 +0700

    Update

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,10 @@
 from typing import Optional
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from openstack import connection, network
 import openstack
 import sys
 
-from websockets import serve
 
 from network import *
 from flavor import *
@@ -39,35 +37,41 @@ async def health():
 
 @app.get("/networks/")
 async def get_networks():
-    networks = conn.list_networks()
-    return [
-        Network(
-            id=net.id,
-            name=net.name,
-            description=net.description,
-            status=net.status,
-            subnet_ids=net.subnet_ids,
-            external=net.is_router_external,
-            created_date=net.created_at,
-            updated_date=net.updated_at,
-        )
-        for net in networks
-    ]
+    try:
+        networks = conn.list_networks()
+        return [
+            Network(
+                id=net.id,
+                name=net.name,
+                description=net.description,
+                status=net.status,
+                subnet_ids=net.subnet_ids,
+                external=net.is_router_external,
+                created_date=net.created_at,
+                updated_date=net.updated_at,
+            )
+            for net in networks
+        ]
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/networks/{network_id}")
 async def get_network(network_id: str):
-    network = conn.get_network(network_id)
-    return Network(
-        id=network.id,
-        name=network.name,
-        description=network.description,
-        status=network.status,
-        subnet_ids=network.subnet_ids,
-        external=network.is_router_external,
-        created_date=network.created_at,
-        updated_date=network.updated_at,
-    )
+    try:
+        network = conn.get_network(network_id)
+        return Network(
+            id=network.id,
+            name=network.name,
+            description=network.description,
+            status=network.status,
+            subnet_ids=network.subnet_ids,
+            external=network.is_router_external,
+            created_date=network.created_at,
+            updated_date=network.updated_at,
+        )
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
 
 
 @app.post("/networks")
@@ -103,7 +107,7 @@ async def create_network(
 
         subnet = conn.create_subnet(
             network_name_or_id=network.id,
-            name=name,
+            name=subnet_name,
             cidr=cidr,
             ip_version=ip_version,
             enable_dhcp=enable_dhcp,
@@ -128,31 +132,37 @@ async def create_network(
 
 @app.get("/subnets/")
 async def get_subnets():
-    subnets = conn.list_subnets()
-    return [
-        Subnet(
+    try:
+        subnets = conn.list_subnets()
+        return [
+            Subnet(
+                id=subnet.id,
+                name=subnet.name,
+                network_id=subnet.network_id,
+                description=subnet.description,
+                cidr=subnet.cidr,
+            )
+            for subnet in subnets
+        ]
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/subnets/{subnet_id}")
+async def get_subnet(subnet_id: str):
+    try:
+        subnet = conn.get_subnet(subnet_id)
+        if subnet is None:
+            return {"error": "Subnet not found"}
+        return Subnet(
             id=subnet.id,
             name=subnet.name,
             network_id=subnet.network_id,
             description=subnet.description,
             cidr=subnet.cidr,
         )
-        for subnet in subnets
-    ]
-
-
-@app.get("/subnets/{subnet_id}")
-async def get_subnet(subnet_id: str):
-    subnet = conn.get_subnet(subnet_id)
-    if subnet is None:
-        return {"error": "Subnet not found"}
-    return Subnet(
-        id=subnet.id,
-        name=subnet.name,
-        network_id=subnet.network_id,
-        description=subnet.description,
-        cidr=subnet.cidr,
-    )
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/flavors/")
@@ -207,7 +217,7 @@ async def create_flavors(
             description=description,
         )
         if flavor is None:
-            return {"error": "Failed to create flavor"}
+            return HTTPException(status_code=500, detail="Flavor is not found")
         return Flavor(
             id=flavor.id,
             name=flavor.name,
@@ -218,7 +228,7 @@ async def create_flavors(
             description="" if flavor.description is None else flavor.description,
         )
     except Exception as e:
-        return {"error": str(e)}
+        return HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/images")
@@ -246,15 +256,18 @@ async def get_image(image_name: str):
 
 @app.get("/instances")
 async def get_instances():
-    instances = conn.list_servers()
-    return [
-        Instance(
-            id=instance.id,
-            name=instance.name,
-            status=instance.status,
-        )
-        for instance in instances
-    ]
+    try:
+        instances = conn.list_servers()
+        return [
+            Instance(
+                id=instance.id,  # type: ignore
+                name=instance.name,  # type: ignore
+                status=instance.status,  # type: ignore
+            )
+            for instance in instances
+        ]
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/instances/{instance_id}")
@@ -276,33 +289,36 @@ async def create_instance(
     flavor_id: str,
     network_id: str,
 ):
-    image = conn.get_image(image_name)
-    if image is None:
-        return {"error": "Image not found"}
-    flavor = conn.get_flavor(flavor_id)
-    if flavor is None:
-        return {"error": "Flavor not found"}
-    network = conn.get_network(network_id)
-    if network is None:
-        return {"error": "Network not found"}
-    user_data = ""
-    with open("userdata.txt", "r") as f:
-        user_data = f.read()
-    instance = conn.create_server(
-        name=name,
-        image=image,
-        flavor=flavor,
-        network=network,
-        auto_ip=True,
-        userdata=user_data,
-    )
+    try:
+        image = conn.get_image(image_name)
+        if image is None:
+            return HTTPException(status_code=404, detail="Image not found")
+        flavor = conn.get_flavor(flavor_id)
+        if flavor is None:
+            return HTTPException(status_code=404, detail="Flavor not found")
+        network = conn.get_network(network_id)
+        if network is None:
+            return HTTPException(status_code=404, detail="Network not found")
+        user_data = ""
+        with open("userdata.txt", "r") as f:
+            user_data = f.read()
+        instance = conn.create_server(
+            name=name,
+            image=image,
+            flavor=flavor,
+            network=network,
+            auto_ip=True,
+            userdata=user_data,
+        )
 
-    conn.wait_for_server(instance)
-    return Instance(
-        id=instance.id,
-        name=instance.name,
-        status=instance.status,
-    )
+        conn.wait_for_server(instance)
+        return Instance(
+            id=instance.id,
+            name=instance.name,
+            status=instance.status,
+        )
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == "__main__":
